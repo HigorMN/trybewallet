b2427e174b97d06dfa6d663a5e5bbaf2cdd00d47
Higor Maranhao <higor.maranhao2000@gmail.com>
2022-09-29 16:15:06 -0300

    input criado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    passoword: '',
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, passoword } = this.state;
+    return (
+      <main>
+        <div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="passoword"
+              placeholder="Senha"
+              data-testid="password-input"
+              value={ passoword }
+              onChange={ this.handleChange }
+            />
+          </div>
+        </div>
+      </main>
+    );
   }
 }
 
