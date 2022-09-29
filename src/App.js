10e1f8f44a2115a0e8db003f5ff5e9746a0f3743
Higor Maranhao <higor.maranhao2000@gmail.com>
2022-09-29 16:03:47 -0300

    css background feito

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import './App.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Hello, TrybeWallet!</div>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
     );
   }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk/es";
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk/es';
 
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk),
   ),
 );
 
