5c7b7d794bc23901a8920ec0fbd7f2d595431709
Higor Maranhao <higor.maranhao2000@gmail.com>
2022-09-30 13:29:51 -0300

    css wallet form concluido

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -26,10 +26,10 @@ class WalletForm extends Component {
     const { description, tag, value, currency, method } = this.state;
     const { coin } = this.props;
     return (
-      <form>
-        <div>
-          <label htmlFor="description">
-            Descrição da despesa
+      <form className="wallet-form-container">
+        <div className="wallet-form-container-2">
+          <label htmlFor="description" className="wallet-form-label-container first-l">
+            <p className="bold wallet-form-text">Descrição da despesa</p>
             <input
               data-testid="description-input"
               type="text"
@@ -37,18 +37,18 @@ class WalletForm extends Component {
               id="description"
               value={ description }
               onChange={ this.handleChange }
+              className="description"
             />
           </label>
-        </div>
-        <div>
-          <label htmlFor="tag">
-            Categoria da despesa
+          <label htmlFor="tag" className="wallet-form-label-container">
+            <p className="bold wallet-form-text">Categoria da despesa</p>
             <select
               name="tag"
               id="tag"
               data-testid="tag-input"
               value={ tag }
               onChange={ this.handleChange }
+              className="tag"
             >
               <option value="Alimentação">Alimentação</option>
               <option value="Lazer">Lazer</option>
@@ -58,44 +58,43 @@ class WalletForm extends Component {
             </select>
           </label>
         </div>
-        <div>
-          <label htmlFor="value">
-            Valor
+        <div className="wallet-form-container-2">
+          <label htmlFor="value" className="wallet-form-label-container label-2">
+            <p className="bold wallet-form-text">Valor</p>
             <input
               data-testid="value-input"
               type="number"
               name="value"
               value={ value }
               id="value"
+              className="value"
               onChange={ this.handleChange }
             />
           </label>
-        </div>
-        <div>
-          <label htmlFor="method">
-            Método de pagamento
+          <label htmlFor="method" className="wallet-form-label-container">
+            <p className="bold wallet-form-text">Método de pagamento</p>
             <select
               name="method"
               id="method"
               value={ method }
               data-testid="method-input"
               onChange={ this.handleChange }
+              className="method"
             >
               <option value="Dinheiro">Dinheiro</option>
               <option value="Cartão de crédito">Cartão de crédito</option>
               <option value="Cartão de débito">Cartão de débito</option>
             </select>
           </label>
-        </div>
-        <div>
-          <label htmlFor="currency">
-            Moeda
+          <label htmlFor="currency" className="wallet-form-label-container center">
+            <p className="bold wallet-form-text">Moeda</p>
             <select
               name="currency"
               id="currency"
               value={ currency }
               data-testid="currency-input"
               onChange={ this.handleChange }
+              className="currency"
             >
               {coin.map((c) => (
                 <option key={ c } value={ c }>{c}</option>
